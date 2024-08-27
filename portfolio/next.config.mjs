786ebe525ef472d469862ade0c4ffd4e05dc96ad
Cjm7603 <cmcloon@ufl.edu>
2024-08-27 08:40:28 -0400

    added next js

---
+++ portfolio/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: "/cjm7603.github.io",
+    images: {
+        unoptimized: true,
+    }
+};
+
+export default nextConfig;
